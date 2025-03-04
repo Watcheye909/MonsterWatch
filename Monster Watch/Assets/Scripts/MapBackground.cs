80fdaf934901f755f7f7cc07a8fc4cc84fd1b94e
TuckerFortified <148375555+TuckerFortified@users.noreply.github.com>
2025-03-04 14:09:35 -0500

    Added slight functionality

--- Monster Watch/Assets/Scripts/MapBackground.cs
+++ Monster Watch/Assets/Scripts/MapBackground.cs
@@ -6,7 +6,8 @@ public class MapBackground : MonoBehaviour
 {
     public static Vector3 SearchPos;
     public static int availableRangers;
-    
+    public GameObject searchPrompt;
+
     void Start()
     {
         availableRangers = 3;
@@ -22,13 +23,16 @@ public class MapBackground : MonoBehaviour
     //When a player clicks on the map.
     public void OnMouseDown()
     {
-        Debug.Log("click");
-        
-        SearchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (availableRangers > 0)
+        if (UIManager.inUI == false)
         {
+            Debug.Log("click");
 
+            SearchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (availableRangers > 0)
+            {
+                searchPrompt.SetActive(true);
+            }
         }
     }
 }
--- Monster Watch/Assets/Scripts/UIManager.cs
+++ Monster Watch/Assets/Scripts/UIManager.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static bool inUI;
 
     public GameObject searchPrompt;
     
     void Start()
     {
+        inUI = false;
         searchPrompt.SetActive(false);
+    }
+    
+    //Checking if the player is using the UI.
+    void Update()
+    {
+        if (searchPrompt.activeInHierarchy)
+        {
+            //Debug.Log("active");
+            inUI = true;
+        }
+        else
+        {
+            inUI = false;
+        }
+    }
+
+    //Functionality for every button that says no.
+    public void NoButton(GameObject prompt)
+    {
+        prompt.SetActive(false);
     }
 
     
-    void Update()
-    {
-        
-    }
+
 }
